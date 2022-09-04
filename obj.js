62c06bff7b1381e1a5aba05289ca757584beae8a
Sardar Ahmed <imsardarahmed@gmail.com>
2022-09-04 08:37:28 +0500

    coding challange one solved

--- obj.js
+++ obj.js
@@ -1,68 +1,22 @@
-//array destructuring
-// let numArray = [1,2,3,4,5,6,7,8,9]
-// let alphabetArray = ["A","B","C","D","E","F","G","H"]
+let marksMass = 78;
+let marksHeight = 1.69;
+let johnsMass = 92;
+let johnsHeight = 1.95;
 
-// let newArray = [...alphabetArray,...numArray]
-// console.log(newArray);
-
-// let [a,b,c,d, ...rest]= numArray
-// console.log(a);
-// console.log(b);
-// console.log(c);
-// console.log(d);
-// console.log(rest);
-
-function sumAndMultiply(a,b) {
-return [a*b, a+b, a/b]    
-}
-let [multiply, sum, division= "no division"] = sumAndMultiply(5,4)
-console.log(multiply);
-console.log(sum);
-console.log(division);
-
-
-
-
-//object destructuring
-
-let personOne = {
-  name: "sardar",
-  age: 33,
-  proffession: "web developer",
-  country:"usa",
-  hobbies: {
-    first: "book reading",
-    second: "playing cricket",
-  },
-  citiesVisited: ["islamabad", "Lahore", "mianwali", "rawalpindi"],
+const findBMI = (mass, height) => {
+   let BMI = mass / (height * height);
+   return BMI
 };
-// let {name, age, hobbies:{first, second, third="sleeping"}} = personOne
-// console.log(first);
-// console.log(second);
-// console.log(third);
 
-// let personTwo = {
-//     name: "zulfi",
-//     age: 43,
-//     proffession: "web developer",
-//     hobbies: {
-//       first: "book reading",
-//       second: "playing tennis",
-//       third : "gardening"
-//     },
-//     citiesVisited: ["islamabad", "Lahore", "mianwali", "rawalpindi"],
-//   };
-//   let personThree = {...personOne, ...personTwo}
-//   console.log(personThree);
+let resultOne = findBMI(marksMass, marksHeight)
+console.log(resultOne);
+let resultTwo = findBMI(johnsMass, johnsHeight)
+console.log(resultTwo);
 
-function user ({name, age, hobbies:{first,second, third="music"},citiesVisited} = personOne){
-    console.log(name, second, third,citiesVisited);
+let markHigherBMI = resultOne > resultTwo
+console.log(markHigherBMI);
+if (resultOne > resultTwo) {
+  console.log(`Mark's BMI ${resultOne} is higher then John's BMI ${resultTwo}`);
+} else {
+  console.log(`John's BMI ${resultTwo} is higher then Mark's BMI ${resultOne}`);
 }
-// user()
-
-
-
-
-
-
-
