069baadfc35d9608a18d4d5c8bf9c50d6d415c5d
Sardar Ahmed <imsardarahmed@gmail.com>
2022-09-04 10:11:55 +0500

    Type Conversion and Type Coersion practiced and coding challang-3 solved

--- obj.js
+++ obj.js
@@ -1,22 +1,54 @@
-let marksMass = 78;
-let marksHeight = 1.69;
-let johnsMass = 92;
-let johnsHeight = 1.95;
+//Type Conversion and Type Coersion
 
-const findBMI = (mass, height) => {
-   let BMI = mass / (height * height);
-   return BMI
-};
+console.log(Number("77") + 25);
+console.log(String(25) + 25);
 
-let resultOne = findBMI(marksMass, marksHeight)
-console.log(resultOne);
-let resultTwo = findBMI(johnsMass, johnsHeight)
-console.log(resultTwo);
+console.log("70" + 25); // 7025
+console.log("70" - 25); // 45
+console.log("70" * 25); // 1750
+console.log("70" / 25); // 2.8
 
-let markHigherBMI = resultOne > resultTwo
-console.log(markHigherBMI);
-if (resultOne > resultTwo) {
-  console.log(`Mark's BMI ${resultOne} is higher then John's BMI ${resultTwo}`);
-} else {
-  console.log(`John's BMI ${resultTwo} is higher then Mark's BMI ${resultOne}`);
+// coding challenge 3
+
+//Data 1
+let dolphinsAvgScre = (96+108+89)/3
+let koalasAvgScre = (88 + 91 + 110)/3
+
+console.log(dolphinsAvgScre , koalasAvgScre );
+if (dolphinsAvgScre > koalasAvgScre && dolphinsAvgScre >= 100 ) {
+ console.log("Dlphins wins the trophy"); 
+} else if (koalasAvgScre > dolphinsAvgScre &&
+   koalasAvgScre >= 100 ) {
+  console.log("Koalas wins the trophy"); 
+} else if (dolphinsAvgScre === koalasAvgScre || dolphinsAvgScre < 100 ||
+   koalasAvgScre < 100  ){
+console.log("The match is draw");
+}
+//Data 2
+// let dolphinsAvgScre = (97+112+101)/3
+// let koalasAvgScre = (109 + 95 + 123)/3
+
+console.log(dolphinsAvgScre , koalasAvgScre );
+if (dolphinsAvgScre > koalasAvgScre && dolphinsAvgScre >= 100 ) {
+ console.log("Dlphins wins the trophy"); 
+} else if (koalasAvgScre > dolphinsAvgScre &&
+   koalasAvgScre >= 100 ) {
+  console.log("Koalas wins the trophy"); 
+} else if (dolphinsAvgScre === koalasAvgScre || dolphinsAvgScre < 100 ||
+   koalasAvgScre < 100  ){
+console.log("The match is draw");
+}
+
+//Data 3
+// let dolphinsAvgScre = (97+112+101)/3
+// let koalasAvgScre = (109 + 95 + 106)/3
+console.log(dolphinsAvgScre , koalasAvgScre );
+if (dolphinsAvgScre > koalasAvgScre && dolphinsAvgScre >= 100 ) {
+ console.log("Dlphins wins the trophy"); 
+} else if (koalasAvgScre > dolphinsAvgScre &&
+   koalasAvgScre >= 100 ) {
+  console.log("Koalas wins the trophy"); 
+} else if (dolphinsAvgScre === koalasAvgScre || dolphinsAvgScre < 100 ||
+   koalasAvgScre < 100  ){
+console.log("The match is draw");
 }
