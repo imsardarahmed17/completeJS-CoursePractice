318c4a8ee4a7d1c0970404ced114c6365b73b3b4
Sardar Ahmed <imsardarahmed@gmail.com>
2022-09-07 13:07:35 +0500

    practiced arrays

--- obj.js
+++ obj.js
@@ -1,47 +1,33 @@
-// Coding Challenge #1
-// Back to the two gymnastics teams, the Dolphins and the Koalas! There is a new 
-// gymnastics discipline, which works differently.
-// Each team competes 3 times, and then the average of the 3 scores is calculated (so 
-// one average score per team).
-// A team only wins if it has at least double the average score of the other team. 
-// Otherwise, no team wins!
-// Your tasks:
-// 1. Create an arrow function 'calcAverage' to calculate the average of 3 scores
-// 2. Use the function to calculate the average for both teams
-// 3. Create a function 'checkWinner' that takes the average score of each team 
-// as parameters ('avgDolhins' and 'avgKoalas'), and then logs the winner 
-// to the console, together with the victory points, according to the rule above. 
-// Example: "Koalas win (30 vs. 13)"
-// 4. Use the 'checkWinner' function to determine the winner for both Data 1 and 
-// Data 2
-// 5. Ignore draws this time
-// Test data:
-// § Data 1: Dolphins score 44, 23 and 71. Koalas score 65, 54 and 49
-// § Data 2: Dolphins score 85, 54 and 41. Koalas score 23, 34 and 27
+// // let friends =["Ali","Asad", "umar"]
+// console.log(friends [friends.length - 1]);
+// let studentsBirthYears=[1991, 2000, 1995]
+// let calcAge =(birtYear)=>{
+//   let age = 2022 - birtYear
+//   console.log(age);
+//   return age
+// }
+// const age1 = calcAge(studentsBirthYears[0])
+// const age2 = calcAge(studentsBirthYears[1])
+// const age3 = calcAge(studentsBirthYears[studentsBirthYears.length - 1])
+// console.log(age1, age2, age3);
+// let ages = [age1, age2, age3]
+// console.log(ages);
 
-//Solution:
+let friends = ["Ali", "Asad", "umar"];
+let poped = friends.pop();
+friends.push("samalik");
+console.log(poped);
 
-const calcAverage = (scoreOne, scoreTwo, scoreThree) => {
-  let avgScore = (scoreOne + scoreTwo + scoreThree) / 3;
-  return avgScore;
-};
+friends.unshift("sam");
+console.log(friends);
+friends.shift();
+console.log(friends);
 
-let dolphinsAvgScore = calcAverage(85, 54, 41);
-console.log(dolphinsAvgScore);
-let koalasAvgScore = calcAverage(23, 34, 27);
-console.log(koalasAvgScore);
+let result = friends.includes("samalik");
+console.log(friends.indexOf("samalik"));
 
-const checkWinner = (dolphinsAvgScore, koalasAvgScore) => {
-  if (dolphinsAvgScore > 2 * koalasAvgScore) {
-    console.log(
-      `Dolphins are the winner scoring ${dolphinsAvgScore} points against ${koalasAvgScore} `
-    );
-  } else if (koalasAvgScore > 2 * dolphinsAvgScore) {
-    console.log(
-      `koalas are the winner scoring ${koalasAvgScore} points against ${dolphinsAvgScore} `
-    );
-  } else {
-    console.log("No team is the winner");
-  }
-};
-checkWinner(dolphinsAvgScore, koalasAvgScore);
+if (friends.includes("saqib")) {
+  console.log("u r present");
+} else {
+  console.log("u r not pesent");
+}
