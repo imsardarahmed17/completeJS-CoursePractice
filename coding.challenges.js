23b5777bee8883d26c842276307c0dfbbd9cfa34
Sardar Ahmed <imsardarahmed@gmail.com>
2022-09-09 14:48:31 +0500

    solved coding challenges and practice complete for js fundamentles

--- coding.challenges.js
+++ coding.challenges.js
@@ -51,48 +51,106 @@
 
 //Solution:
 
-const markInfo = {
-  fullName: "Mark Miller",
-  mass: 78,
-  height: 1.69,
-  calcBMI: function () {
-    this.BMI = this.mass / (this.height * this.height);
-    return this.BMI;
-  },
-};
-markInfo.calcBMI();
+// const markInfo = {
+//   fullName: "Mark Miller",
+//   mass: 78,
+//   height: 1.69,
+//   calcBMI: function () {
+//     this.BMI = this.mass / (this.height * this.height);
+//     return this.BMI;
+//   },
+// };
+// markInfo.calcBMI();
 
-const johnInfo = {
-  fullName: "John Smith",
-  mass: 92,
-  height: 1.95,
-  calcBMI: function () {
-    this.BMI = this.mass / this.height ** 2;
-    return this.BMI;
-  },
-};
-johnInfo.calcBMI();
+// const johnInfo = {
+//   fullName: "John Smith",
+//   mass: 92,
+//   height: 1.95,
+//   calcBMI: function () {
+//     this.BMI = this.mass / this.height ** 2;
+//     return this.BMI;
+//   },
+// };
+// johnInfo.calcBMI();
 
-if (markInfo.BMI > johnInfo.BMI) {
-  console.log(
-    `Mark's BMI (${markInfo.BMI} )is higher then John's (${johnInfo.BMI})`
-  );
-} else if (johnInfo.BMI > markInfo.BMI) {
-  console.log(
-    `John's BMI (${johnInfo.BMI}) is higher then Mark's (${markInfo.BMI})`
-  );
+// if (markInfo.BMI > johnInfo.BMI) {
+//   console.log(
+//     `Mark's BMI (${markInfo.BMI} )is higher then John's (${johnInfo.BMI})`
+//   );
+// } else if (johnInfo.BMI > markInfo.BMI) {
+//   console.log(
+//     `John's BMI (${johnInfo.BMI}) is higher then Mark's (${markInfo.BMI})`
+//   );
+// }
+
+// //practice:
+
+// let player = {
+//   name: "baber",
+//   score: 100,
+//   balls: 90,
+//   findStrikeRate: function () {
+//     this.srikeRate = (this.score / this.balls) * 100;
+//     return this.srikeRate;
+//   },
+// };
+// player.findStrikeRate();
+// console.log(player.srikeRate);
+
+// Coding Challenge #4
+// Let's improve Steven's tip calculator even more, this time using loops!
+// Your tasks:
+// 1. Create an array 'bills' containing all 10 test bill values
+// 2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+// 3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate
+// tips and total values (bill + tip) for every bill value in the bills array. Use a for
+// loop to perform the 10 calculations!
+// Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+// Hints: Call ‘calcTip ‘in the loop and use the push method to add values to the
+// tips and totals arrays �
+// Bonus:
+// 4. Bonus: Write a function 'calcAverage' which takes an array called 'arr' as
+// an argument. This function calculates the average of all numbers in the given
+// array. This is a difficult challenge (we haven't done this before)! Here is how to
+// solve it:
+// 4.1. First, you will need to add up all values in the array. To do the addition,
+// start by creating a variable 'sum' that starts at 0. Then loop over the
+// array using a for loop. In each iteration, add the current value to the
+// 'sum' variable. This way, by the end of the loop, you have all values
+// added together
+// 4.2. To calculate the average, divide the sum you calculated before by the
+// length of the array (because that's the number of elements)
+// 4.3. Call the function with the 'totals' array
+
+//solution:
+
+let bills = [22, 295, 176, 440, 37, 105, 10];
+let tips = [];
+let totals = [];
+const calcTip = (bill) => {
+  return (tip = bill >= 50 && bill <= 300 ? 0.15 * bill : 0.2 * bill);
+};
+
+for (let i = 0; i < bills.length; i++) {
+  const tip = calcTip(bills[i]);
+  tips.push(tip);
+  totals.push(tip + bills[i]);
 }
 
-//practice:
+console.log(bills, tips, totals);
 
-let player = {
-  name: "baber",
-  score: 100,
-  balls: 90,
-  findStrikeRate: function () {
-    this.srikeRate = (this.score / this.balls) * 100;
-    return this.srikeRate;
-  },
+//Bonus:
+
+// let arr =[12,56,45,78,15,46,49,26,16,85]
+
+let calcAverage = function (arr) {
+  sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    // sum = sum + arr[i]
+    sum += arr[i];
+  }
+  return sum / arr.length;
 };
-player.findStrikeRate();
-console.log(player.srikeRate);
+let avg = Math.trunc(calcAverage(tips));
+
+console.log(avg);
